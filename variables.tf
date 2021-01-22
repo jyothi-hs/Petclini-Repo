e3da2401cbdfa250940574c9eab75e53f3c1fedc
root <root@mtadmin123.uyndh22ztjzurh52xohl44o4ch.cx.internal.cloudapp.net>
2021-01-22 17:45:20 +0000

    terraform

---
+++ variables.tf
@@ -0,0 +1,14 @@
+
+variable "key_name" {
+  default = "terraform"
+}
+
+
+variable "pvt_key" {
+  default = "/root/.ssh/terraform.pem"
+}
+
+
+variable "sg_id" {
+  default = "sg-0d7ed17d16a5bb165"
+}
